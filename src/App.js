a6b21fd8f73c8d2297a5d9dce62491ab08975733
atulbansalofficial <atulbansal9@gmail.com>
2022-02-03 17:24:42 +0530

    update axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-// import Todo from "./Todo";
+
 import TableInput from "./TableInput";
 
 function App(props) {
-  function ParentAlert(data) {
-    console.log("Data", data);
-  }
   return (
     <div>
-      {/* <Todo alert={ParentAlert} /> */}
       <TableInput />
     </div>
   );
--- src/TableInput.js
+++ src/TableInput.js
@@ -1,43 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+const axios = require("axios");
 
 function TableInput() {
+  axios
+    .get(
+      "http://ec2-3-109-108-73.ap-south-1.compute.amazonaws.com/api/v1/employer/job?luid=61f23762a6b6f90afa49cd4c"
+    )
+    .then(function (response) {
+      // handle success
+      console.log(response.data.map((item)=>{
+        console.log();
+        
+      }));
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
   return (
-    <div className="container">
+    <div>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">Skill</th>
-            <th scope="col">Experience (years)</th>
-            <th scope="col" className="float-end">
-              ADD/Delete
-            </th>
+            <th scope="col">S.No</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>
-              <input type="text" placeholder="Java" />
-            </td>
-            <td>
-              <input type="text" placeholder="2" />
-            </td>
-            <td>
-              <button
-                type="button"
-                value={"Add"}
-                className="btn float-end  btn-outline-secondary "
-              >
-                <i className="fa fa-plus" aria-hidden="true"></i>
-              </button>
-            </td>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
           </tr>
           <tr>
+            <th scope="row">2</th>
             <td>Jacob</td>
             <td>Thornton</td>
             <td>@fat</td>
           </tr>
           <tr>
-            <td colspan="2">Larry the Bird</td>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
             <td>@twitter</td>
           </tr>
         </tbody>
