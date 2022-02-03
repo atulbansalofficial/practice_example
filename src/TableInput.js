d20cacf4b9ac900e68af1991e197068b82a1ed72
atulbansalofficial <atulbansal9@gmail.com>
2022-02-04 01:39:11 +0530

    update axios

--- src/TableInput.js
+++ src/TableInput.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 const axios = require("axios");
 
 function TableInput() {
+  const [jobTital, setjobTital] = useState([]);
+  console.log(jobTital);
+
   axios
     .get(
       "http://ec2-3-109-108-73.ap-south-1.compute.amazonaws.com/api/v1/employer/job?luid=61f23762a6b6f90afa49cd4c"
     )
     .then(function (response) {
       // handle success
-      console.log(response.data.map((item)=>{
-        console.log();
-        
-      }));
+      console.log(response.data[0]);
     })
     .catch(function (error) {
       // handle error
@@ -20,15 +20,22 @@ function TableInput() {
     .then(function () {
       // always executed
     });
+
   return (
     <div>
       <table className="table">
         <thead>
           <tr>
             <th scope="col">S.No</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Job Title</th>
+            <th scope="col">Published On</th>
+            <th scope="col">Matching Candidates</th>
+            <th scope="col">Candidates Applied(*)</th>
+            <th scope="col">Skills+Rating</th>
+            <th scope="col">Location</th>
+            <th scope="col">Status</th>
+            <th scope="col">Updated On</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -37,21 +44,16 @@ function TableInput() {
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
           </tr>
         </tbody>
       </table>
+      {}
     </div>
   );
 }
